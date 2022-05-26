d376795e87263d118a4392705bebbe29ea26810b
Rachel Kelly <racheldk126@gmail.com>
2022-05-26 14:09:47 -0700

    clear button js

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
 let buttons = document.getElementsByTagName("button")
 console.log(buttons)
+const display = document.getElementById("display")
+const clear = document.getElementById("C")
 
 for (let button of buttons) {
-    button.addEventListener("click", function (event){
+    button.addEventListener("click", function (event) {
         console.log(event.target.textContent);
         let newElement = document.createElement('p');
-        newElement.classList.add('display');
         newElement.innerText = event.target.textContent;
         display.appendChild(newElement);
     })
 }
 
-// clear button
+clear.addEventListener("click", function (event){
+    console.log(event.target);
+    display.innerHTML = "";
+})
+
 
 
 // math part  from equal button
