b8cd947ca1826bbd072c6988b2f1a74d63306438
Rachel Kelly <racheldk126@gmail.com>
2022-05-26 13:08:58 -0700

    button clicked displayed on page

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
-let buttons = document.querySelectorAll(".button")
+let buttons = document.getElementsByTagName("button")
 console.log(buttons)
 
 for (let button of buttons) {
     button.addEventListener("click", function (event){
-        console.log(event.target.textContent)
+        console.log(event.target.textContent);
+        let newElement = document.createElement('p');
+        newElement.classList.add('display');
+        newElement.innerText = event.target.textContent;
+        display.appendChild(newElement);
     })
 }
+
+// clear button
+
+
+// math part  from equal button
+
+
+
+
